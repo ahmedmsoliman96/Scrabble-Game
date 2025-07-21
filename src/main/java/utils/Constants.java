192964ce5ebd81642bf9a8966ccd7dca6fbad571
ahmedmsoliman96 <27621581+ahmedmsoliman96@users.noreply.github.com>
2025-07-21 18:44:20 +0300

    created Constants class to hold static variables used globally

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,38 @@
+package utils;
+
+public final class Constants {
+    private Constants() {}
+    public static final byte ROWS_COUNT = 15;
+    public static final byte COLUMNS_COUNT = 15;
+    public static final char [] alphabets = {
+            'A',
+            'B',
+            'C',
+            'D',
+            'E',
+            'F',
+            'G',
+            'H',
+            'I',
+            'J',
+            'K',
+            'L',
+            'M',
+            'N',
+            'O',
+            'P',
+            'Q',
+            'R',
+            'S',
+            'T',
+            'U',
+            'V',
+            'W',
+            'X',
+            'Y',
+            'Z'
+    };
+
+
+
+}
