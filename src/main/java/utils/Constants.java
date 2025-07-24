4e2112826ba3482e3b2ac1699bc94dd68efc0530
ahmedmsoliman96 <27621581+ahmedmsoliman96@users.noreply.github.com>
2025-07-24 22:44:50 +0300

    Add BLANK_TILE in Constants class

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -4,6 +4,7 @@ public final class Constants {
     private Constants() {}
     public static final byte ROWS_COUNT = 15;
     public static final byte COLUMNS_COUNT = 15;
+    public static final char BLANK_TILE ='_';
     public static final char [] alphabets = {
             'A',
             'B',
